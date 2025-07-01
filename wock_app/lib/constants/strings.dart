069be4909bc36c4c2c6319b906f69960c6a9b4c2
Lilio Tapia <liliobenjamin.t@gmail.com>
2025-06-30 23:51:54 -0400

    Add wock_app/lib/constants/strings.dart 1751341913508

---
+++ wock_app/lib/constants/strings.dart
@@ -0,0 +1,30 @@
+class AppStrings {
+  // Auth
+  static const welcome = 'Welcome to Wock';
+  static const login = 'Login';
+  static const scanQR = 'Scan QR Code';
+  static const email = 'Email';
+  static const password = 'Password';
+  static const forgotPassword = 'Forgot Password?';
+  static const dontHaveAccount = "Don't have an account?";
+  static const signUp = 'Sign Up';
+  static const enterEmail = 'Enter your email';
+  static const enterPassword = 'Enter your password';
+
+  // Home
+  static const greeting = '¡Hola';
+  static const notificationTitle = '¡Hey Elocker!';
+  static const notificationMessage = 'Tienes un nuevo paquete';
+  static const reviewNow = 'Revisar ahora';
+  
+  // Navigation
+  static const home = 'Inicio';
+  static const search = 'Buscar';
+  static const gallery = 'Galería';
+  static const profile = 'Perfil';
+  
+  // Actions
+  static const statistics = 'Estadísticas';
+  static const community = 'Comunidad';
+  static const payments = 'Pagos';
+}
